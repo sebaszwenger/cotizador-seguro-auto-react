541a4842239a8a9bcdb165246730cc6efa261c6c
sebaszwenger <sebaszwenger@gmail.com>
2021-09-07 21:04:11 -0300

    Merge branch 'main' of https://github.com/sebaszwenger/cotizador-seguro-auto

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
+function App() {
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de seguros de autos" />
+      <ContenedorFormulario></ContenedorFormulario>
+    </Contenedor>
+  );
+}
+
+export default App;
